a7e8458a879db2fa095c780e8e34fd72eb853dac
Brynn Brady <brynnbrady@Brynns-MacBook-Air.local>
2021-10-12 21:44:03 -0700

    Dictionaries Exercises

--- project0/mtnHeights.py
+++ project0/mtnHeights.py
@@ -1,6 +1,11 @@
 mtnHeights = {"Mount Everest" : 8848, "Cho Oyu" : 8188, "Dhaulagiri" : 8167, "Manaslu" : 8163, "Broad Pak" : 8051}
 for mtn, height in mtnHeights.items():
     print(mtn)
+print()
 
 for mtn, height in mtnHeights.items():
     print(height)
+print()
+
+for mtn, height in mtnHeights.items():
+    print(mtn, "is", height, "meters tall")
